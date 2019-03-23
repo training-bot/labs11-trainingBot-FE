093003c0b626ee05b8c4c7ae953c84bb448b1170
Michael Landers <landers.mike@gmail.com>
2019-03-23 13:37:06 -0700

    Added edit and delete account buttons

--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography,
+	withStyles
+} from '@material-ui/core';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import AppBar from '../AppBar/AppBar';
 import styled from 'styled-components';
 
@@ -79,6 +81,14 @@ function Profile(props) {
 								</Typography>
 							</CardContent>
 						</CardActionArea>
+						<CardActions>
+							<Button size='small' color='primary'>
+								Edit
+							</Button>
+							<Button size='small' color='secondary'>
+								Delete Account
+							</Button>
+						</CardActions>
 					</Card>
 				</div>
 			</Container>
