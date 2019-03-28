0a216d762af543c903ee30ae361a1d20a62ffecd
Brandon Lent <blent1050@gmail.com>
2019-03-28 17:59:49 -0400

    restructured profile to bring in state

--- client/src/components/Profile/ProfileView.js
+++ client/src/components/Profile/ProfileView.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import {Elements, StripeProvider} from 'react-stripe-elements';
-import Authentication from '../authenticate/authenticate';
-import {getUserProfile} from '../../Auth/Auth';
+import React from "react";
+//Stripe
+import { Elements, StripeProvider } from "react-stripe-elements";
+import CheckoutForm from "../Stripe/CheckoutForm";
 
-import AppBar from '../AppBar/AppBar';
+//Authentication
+import Authenticate from "../authenticate/authenticate";
 
-import {logout} from '../../Auth/Auth';
-import CheckoutForm from '../Stripe/CheckoutForm';
-//Axios
-import axios from 'axios';
-
+//State Management
+import { getUser } from "../../store/actions/userActions";
+import { connect } from "react-redux";
 //Styling
+import AppBar from "../AppBar/AppBar";
+
 import {
   Button,
   Card,
@@ -18,22 +19,22 @@ import {
   // CardContent,
   CardMedia,
   Typography,
-  withStyles,
-} from '@material-ui/core';
-import styled from 'styled-components';
+  withStyles
+} from "@material-ui/core";
+import styled from "styled-components";
 
 const styles = {
   card: {
     maxWidth: 800,
-    margin: '0 auto',
+    margin: "0 auto"
   },
   cardContent: {
-    backgroundColor: '#E8E9EB',
+    backgroundColor: "#E8E9EB"
   },
   media: {
     height: 200,
-    width: 200,
-  },
+    width: 200
+  }
 };
 
 const Container = styled.div`
@@ -41,84 +42,58 @@ const Container = styled.div`
 `;
 
 class ProfileView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userProfile: [],
-      user: {},
-    };
-  }
-
-  handleLogout = e => {
-    e.preventDefault();
-    logout();
-    this.props.history.push('/');
-  };
-
   componentDidMount() {
-    //Gets the user profile and assigns it to state
-    const user = JSON.parse(localStorage.getItem('Profile'));
-    this.setState({userProfile: user});
-    this.getProfile();
+    this.props.getUser();
   }
-  //Gets the users Profile
-  getProfile = () => {
-    getUserProfile(() => {
-      const userData = JSON.parse(localStorage.getItem('Profile'));
-      const {email, name} = userData;
-      axios
-        .post('https://labs11-trainingbot-dev.herokuapp.com/api/auth', {
-          email,
-          name,
-        })
-        .then(res => {
-          let userData = res.data.user;
-          this.setState({user: {...userData}, doneLoading: true});
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-  };
-
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
-      <StripeProvider apiKey="pk_test_L76yOnUDjq2cNP8heEH9MkpA00Ktyd3MYn">
+      <StripeProvider apiKey='pk_test_L76yOnUDjq2cNP8heEH9MkpA00Ktyd3MYn'>
         <Container>
           <AppBar />
-          <div className="profile-area">
+          {/* <div className='profile-area'>
             <Card className={classes.card}>
-              <Typography gutterBottom variant="h5" component="h1">
-                {this.state.userProfile.name}
+              <Typography gutterBottom variant='h5' component='h1'>
+                {this.props.userProfile.name}
               </Typography>
 
               <CardMedia
                 className={classes.media}
-                image={this.state.userProfile.picture}
-                title="Contemplative Reptile"
+                image={this.props.userProfile.picture}
+                title='Contemplative Reptile'
               />
-              <Typography gutterBottom variant="h5" component="h5">
-                {this.state.user.email}
+              <Typography gutterBottom variant='h5' component='h5'>
+                {this.props.user.email}
               </Typography>
               <CardActions>
-                <Button size="small" color="primary">
+                <Button size='small' color='primary'>
                   Edit
                 </Button>
-                <Button size="small" color="secondary">
+                <Button size='small' color='secondary'>
                   Delete Account
                 </Button>
               </CardActions>
             </Card>
           </div>
           <Elements>
-            <CheckoutForm user={this.state.user} />
-          </Elements>
+            <CheckoutForm user={this.props.user} />
+          </Elements> */}
         </Container>
       </StripeProvider>
     );
   }
 }
 
-export default withStyles(styles)(Authentication(ProfileView));
+const mapStateToProps = state => {
+  return {
+    userProfile: state.userReducer.userProfile
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    getUser
+  }
+)(withStyles(styles)(Authenticate(ProfileView)));
